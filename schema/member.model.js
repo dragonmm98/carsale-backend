dc008e90ef95d80c696d1240be129da9c166c51d
dragonmm98 <3d.dragonmm@98gmail.com>
2023-10-13 21:30:00 +0900

    fix: member schema

--- schema/member.model.js
+++ schema/member.model.js
@@ -34,7 +34,7 @@ const memberSchema = new mongoose.Schema ({
             message: "{VALUE} is not among permitted values"
         }
     },
-    mb_full_name:{
+    mb_description:{
         type:String,
         required:false
     },
@@ -50,9 +50,38 @@ const memberSchema = new mongoose.Schema ({
         type:Number,
         required:false,
         default:0
-    }
-
-});
+    },
+    mb_top:{
+        type:String,
+        required:false,
+        default:'N',
+        enum :{
+            values: ordernary_enums,
+            message:"{VALUE} is not among permitted values"
+        }
+    },
+    mb_views:{
+        type : Number,
+        required: false,
+        default: 0
+    },
+    mb_likes:{
+        type : Number,
+        required: false,
+        default: 0
+    },
+    mb_follow_cnt:{
+        type : Number,
+        required: false,
+        default: 0
+    },
+    mb_subscriber_cnt:{
+        type : Number,
+        required: false,
+        default: 0
+    },
+      
+}, {timestamps:true})  // Mongo db adds createdAt and updatedAt);
 
 
 module.exports = mongoose.model("Member",memberSchema);
