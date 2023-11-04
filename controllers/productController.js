dc3201199134d82faa6140499aec187356b7d540
dragonmm98 <3d.dragonmm@98gmail.com>
2023-11-04 18:42:34 +0900

    feat: restaurant product menu page for admins done

--- controllers/productController.js
+++ controllers/productController.js
@@ -27,7 +27,7 @@ productController.addNewProduct = async (req,res) => {
         const result = await product.addNewProductData(data,req.member);
 
         const html = `<script>
-                    alert(new dish added successfully);
+                    alert('new dish added successfully');
                      window.location.replace('/resto/products/menu');
                     </script>`
                     res.end(html);
@@ -43,9 +43,9 @@ productController.addNewProduct = async (req,res) => {
 productController.updateChosenProduct = async (req,res) => {
     try {
         console.log ("POST: cont/updateChosenProduct");
-        const product  = new Product();
+        const product  = new Product(); 
         const id = req.params.id;
-        const result = await  product.updateChosenProduct(id,req.body,req.member._id);
+        const result = await product.updateChosenProduct(id,req.body,req.member._id);
          await res.json({state: "success", data: result}); 
     }   catch (err) {
         console.log (`ERROR, cont/updateChosenProduct ${err.message}`);
---
+++ public/addproduct.js
@@ -0,0 +1,74 @@
+$(function(){
+    
+    $(".product_collection").on("change",() =>{
+        const selected_value = $(".product_collection").val();
+        if(selected_value === 'drink') {
+            $("#product_volume").show();
+            $("#product_size").hide();
+        } else {
+            $("#product_volume").hide();
+            $("#product_size").show();
+        }
+    })
+
+ $(".hiding_btn").on("click",() =>{
+    $(".dish_container").slideToggle(500);
+    $(".hiding_btn").css("display","none");
+ })
+   // Product Status Changer
+   $(".new_product_status").on("change", async function(e){
+    const id= e.target.id;
+    const product_status = $(`#${id}.new_product_status`).val();
+     try{
+        const response = await axios.post(`/resto/products/edit/${id}`,{id: id, product_status: product_status});
+        const result = response.data;
+        console.log("result:", result);
+
+        if (result.state == 'success') {
+            alert('success');
+            location.reload();
+        } else {
+            alert (result.message);
+        }
+     } catch (err) {
+        console.log('updateChosenProduct err:', err);
+     }
+
+   })
+  
+});
+
+function validateForm() {
+    const restaurant_mb_id = $(".restaurant_mb_id").val(),
+          product_name = $(".product_name").val(),
+          product_price = $(".product_price").val(),
+          product_left_cnt = $(".product_left_cnt").val(),
+          product_collection = $(".product_collection").val(),
+          product_status = $(".product_status").val(),
+          product_description = $(".product_description").val()  
+
+          if(restaurant_mb_id == '' || product_name == '' || product_price == '' || product_left_cnt == '' ||
+          product_collection == '' || product_description =='' || product_status == '') {
+            alert ("Please fill all the fields");
+            return false;
+          } else return true;
+}
+
+function previewImgHandler(input, order) {
+    const image_class_name = input.className;
+    const file = $(`.${image_class_name}`).get(0).files[0],
+        fileType= file['type'],
+        validImageTypes= ['image/jpg','image/jpeg','image/png'];
+
+        if (!validImageTypes.includes(fileType)) {
+            alert ('Please upload only allowed pictures (jpg,jpeg,png)');
+        } else {
+            if(file) {
+                let reader = new FileReader();
+                reader.onload = function (){
+                    $(`#image_section_${order}`).attr("src", reader.result); 
+                }
+                reader.readAsDataURL(file);
+            }
+        }
+}
