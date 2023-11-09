9c30c4d077d1642f2ff35305704c024bc6d029dd
dragonmm98 <3d.dragonmm@98gmail.com>
2023-11-09 14:11:47 +0900

    feat: completed develop all restaurants controlpage

--- public/allrestaurant.js
+++ public/allrestaurant.js
@@ -14,5 +14,22 @@ $(function(){
         })
         .catch(err => console.log(err));
 
-    })
-}) 
+    });
+
+     // Manipulate Restaurant status 
+     $(".mb_status").on('change', function(e) {
+        const id = e.target.id;
+        const mb_status = $(`#${id}.mb_status`).val();
+        console.log("mb_status", mb_status)
+        
+        axios.post("/resto/all-restaurants/update",{id: id, mb_status: mb_status})
+        .then(response => {
+            const result = response.data;
+            if (result.state === 'success') alert ('Successfully updated!');
+            else alert (result.message);
+
+        })
+        .catch(err => console.log(err));
+
+    });
+});
