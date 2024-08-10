226085f1bff1e0904625e6a7787b595f489322ce
dragonmm98 <3d.dragonmm@98gmail.com>
2024-08-11 03:53:52 +0900

    feat: config.js file starting script creation

---
+++ process.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+    apps : [{
+      name: "Calne",
+      cwd: "./",
+      script: "./server.js",
+      watch: true,
+      env_development: {
+        NODE_ENV: "development",
+    },
+     env_production: {
+        NODE_ENV: "production",
+      },
+      instances: "2",
+      exec_mode: "cluster",
+    }, 
+   ],
+  };
